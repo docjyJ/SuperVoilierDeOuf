eed5464daf6c6ed50ca1eaf761f4b8aa38800a08
Jean-Yves S <jysaintloubert+github@gmail.com>
2023-11-10 16:35:56 +0100

    Initial Gouv

---
+++ Services/Gouvernail.c
@@ -0,0 +1,25 @@
+#include "Gouvernail.h"
+#include "stm32f10x.h"
+#include "Driver_GPIO.h"
+#include "Driver_TIMER.h"
+
+void MyGouv_Init() {
+	MyGPIO_Init(GPIOA, 1, AltOut_Ppull); // TODO: LA PIN ?
+	MyGPIO_Init(GPIOA, 2, Out_Ppull);
+	
+	MyTimer_Base_Init(TIM2, 50, 71); // TODO: FRECANCE ?
+	MyTimer_PWM(TIM2, 2);
+	MyTimer_PWM_Cycle(TIM2, 2, 0);
+	MyTimer_Base_Start(TIM2);
+}
+
+void MyGouv_Command(int8_t cmd) {
+	if (cmd < 0) {
+		MyGPIO_Set(GPIOA, 2);
+		MyTimer_PWM_Cycle(TIM2, 2, cmd * 250);
+	}
+	else {
+		MyGPIO_Reset(GPIOA, 2);
+		MyTimer_PWM_Cycle(TIM2, 2, - cmd * 250);
+	}
+}
---
+++ Services/Gouvernail.h
@@ -0,0 +1,8 @@
+#ifndef MYGOUV_H
+#define MYGOUV_H
+#include "stm32f10x.h"
+
+void MyGouv_Init(void);
+void MyGouv_Command(int8_t cmd);
+
+#endif
