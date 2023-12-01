392dc8b1b33e48c55c3250368a321277ae483f7c
Jean-Yves S <jysaintloubert+github@gmail.com>
2023-12-01 09:52:38 +0100

    SuperGouv

--- Keil/Sources/main.c
+++ Keil/Sources/main.c
@@ -1,26 +1,11 @@
 #include "stm32f10x.h"
-#include "Driver_TIMER.h"
-#include "Driver_GPIO.h"
-#include "Driver_ADC.h"
-#include "Driver_USART.h"
 #include "Gouvernail.h"
-
-void Blop1(char test){
-	test++;
-	MyUsart_Send(USART3, 'O');
-}
 
 
 int main() {
-	MyGPIO_Init ( GPIOB,  10, AltOut_Ppull_10);
-	MyGPIO_Init ( GPIOB,  11, In_PullUp);
-	
-	MyUsart_Base_Init (USART3, 9600);
-	MyUsart_ActiveIT (USART3, 2, Blop1);
-	
-	
+	MyGouv_Init();
 	
 	while(1){
-		MyUsart_Send(USART3, 'i');
+		__asm__ ("nop");
 	}
 }
--- Services/Gouvernail.c
+++ Services/Gouvernail.c
@@ -3,6 +3,8 @@
 #include "Driver_GPIO.h"
 #include "Driver_TIMER.h"
 
+#include "Driver_USART.h"
+
 void MyGouv_Init() {
 	MyGPIO_Init(GPIOA, 1, AltOut_Ppull); // TODO: LA PIN ?
 	MyGPIO_Init(GPIOA, 2, Out_Ppull);
@@ -11,6 +13,12 @@ void MyGouv_Init() {
 	MyTimer_PWM(TIM2, 2);
 	MyTimer_PWM_Cycle(TIM2, 2, 0);
 	MyTimer_Base_Start(TIM2);
+	
+	MyGPIO_Init ( GPIOB,  10, AltOut_Ppull_10);
+	MyGPIO_Init ( GPIOB,  11, In_PullUp);
+	
+	MyUsart_Base_Init (USART3, 9600);
+	MyUsart_ActiveIT (USART3, 2, MyGouv_Command);
 }
 
 void MyGouv_Command(int8_t cmd) {
