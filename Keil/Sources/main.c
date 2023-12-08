5569e8611215103c7aab800918834958898c6770
Jean-Yves S <jysaintloubert+github@gmail.com>
2023-12-08 16:14:06 +0100

    Update main.c

--- Keil/Sources/main.c
+++ Keil/Sources/main.c
@@ -2,24 +2,11 @@
 #include "Driver_TIMER.h"
 #include "Driver_GPIO.h"
 #include "Driver_ADC.h"
-#include "Girouette.h"
-#include "Voiles.h"
-int angle;
+
 
 int main() {
-	
-	MyGPIO_Init ( GPIOA,  6, In_Floating);
-	MyGPIO_Init ( GPIOA,  7, In_Floating);
-	MyGPIO_Init ( GPIOB,  1, In_Floating);
 
-	Config_Girouette();
-	while(!get_start());
-	
-	Init_PWM_Voilier();
-	
 	while(1){
-		angle = getAngle();
-		Change_PWM_Cycle(angle_PWM(angle));
-		
+		__asm__ ("nop");
 	}
 }
