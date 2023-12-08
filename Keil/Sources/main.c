47f32be11126d7ecf421f1d5da65625b6c4a5277
BoireauJInsa <127937383+BoireauJInsa@users.noreply.github.com>
2023-12-08 14:35:43 +0100

    files model edit

--- Keil/Sources/main.c
+++ Keil/Sources/main.c
@@ -1,11 +1,16 @@
 #include "stm32f10x.h"
 #include "Gouvernail.h"
+#include "GestionBat.h"
 
+uint32_t Bat = 0;
 
 int main() {
 	MyGouv_Init();
 	
+	
 	while(1){
+		MyBatConfigPin();
+		Bat = MyBatGetVoltage();
 		__asm__ ("nop");
 	}
 }
