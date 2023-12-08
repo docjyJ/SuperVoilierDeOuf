54aae40d84d2fd0222c95c39001d6d45dc50270a
Jean-Yves S <jysaintloubert+github@gmail.com>
2023-12-08 14:44:31 +0100

    Update main.c

--- Keil/Sources/main.c
+++ Keil/Sources/main.c
@@ -1,16 +1,10 @@
 #include "stm32f10x.h"
-#include "Gouvernail.h"
-#include "GestionBat.h"
 
-uint32_t Bat = 0;
 
 int main() {
-	MyGouv_Init();
 	
 	
 	while(1){
-		MyBatConfigPin();
-		Bat = MyBatGetVoltage();
 		__asm__ ("nop");
 	}
 }
